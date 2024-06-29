22ec38d1722027e34aa2d957126b3891510f48c9
bibikov nikita <bibikovnikita777@gmail.com>
2024-06-29 13:54:30 +0600

    Created the project structure, initialized git, connected axios. Gave names to the components. divided into component and container

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import HomePage from './containers/HomePage';
+import AddQuotePage from './containers/AddQuotePage';
+import EditQuotePage from './containers/EditQuotePage';
+import CategoryPage from './containers/CategoryPage';
+
+const App: React.FC = () => {
+    return (
+<>
+
+</>
+    )
+export default App;
